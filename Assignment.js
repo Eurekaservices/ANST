8039e23bd58c7b158394caac2470765a29273994
Eurekaservices <dareparkinson@gmail.com>
2025-03-01 02:19:52 +0100

    Initial commit

---
+++ Assignment.js
@@ -0,0 +1,19 @@
+// Declare a variable
+
+let num1 = 34;
+let num2 = 5;
+
+// perform operation and store results
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+//print results
+console.log ("addition: " + num1 + " + " + num2 + " = " + addition);
+console.log ("addition: " + num1 + " - " + num2 + " = " + subtraction);
+console.log ("addition: " + num1 + " * " + num2 + " = " + multiplication);
+console.log ("addition: " + num1 + " / " + num2 + " = " + division);
+console.log ("addition: " + num1 + " % " + num2 + " = " + modulus);
+
